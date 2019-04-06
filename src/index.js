9338eeec4f5868c5e023bfa101b45c83c918d34d
Gara64 <paul@cozycloud.cc>
2019-04-06 19:52:01 +0200

    feat: ENSAP konnector

--- src/index.js
+++ src/index.js
@@ -1,129 +1,86 @@
 const {
   BaseKonnector,
   requestFactory,
-  signin,
-  scrape,
   saveBills,
-  log,
-  utils
+  log
 } = require('cozy-konnector-libs')
 const request = requestFactory({
-  // the debug mode shows all the details about http request and responses. Very useful for
-  // debugging but very verbose. That is why it is commented out by default
-  // debug: true,
-  // activates [cheerio](https://cheerio.js.org/) parsing on each page
-  cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
   json: false,
-  // this allows request-promise to keep cookies between requests
   jar: true
 })
 
-const VENDOR = 'template'
-const baseUrl = 'http://books.toscrape.com'
+const VENDOR = 'Ensap'
+const baseUrl = 'https://ensap.gouv.fr'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
 async function start(fields) {
   log('info', 'Authenticating ...')
   await authenticate(fields.login, fields.password)
   log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
-  log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
   log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($)
-
-  // here we use the saveBills function even if what we fetch are not bills, but this is the most
-  // common case in connectors
-  log('info', 'Saving data to Cozy')
+  const documents = await parseDocuments()
   await saveBills(documents, fields, {
-    // this is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
+    identifiers: ['ddfip']
   })
 }
 
-// this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
 function authenticate(username, password) {
-  return signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // the validate function will check if the login request was a success. Every website has
-    // different ways respond: http status code, error message in html ($), http redirection
-    // (fullResponse.request.uri.href)...
-    validate: (statusCode, $, fullResponse) => {
-      log(
-        'debug',
-        fullResponse.request.uri.href,
-        'not used here but should be usefull for other connectors'
-      )
-      // The login in toscrape.com always works excepted when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
-        return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
-      }
-    }
+  return request({
+    uri: `${baseUrl}/authentification`,
+    method: 'POST',
+    formData: { identifiant: username, secret: password }
   })
 }
 
-// The goal of this function is to parse a html page wrapped by a cheerio instance
-// and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
-function parseDocuments($) {
-  // you can find documentation about the scrape function here :
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
-  const docs = scrape(
-    $,
-    {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
-      },
-      amount: {
-        sel: '.price_color',
-        parse: normalizePrice
-      },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
-      }
-    },
-    'article'
-  )
-  return docs.map(doc => ({
-    ...doc,
-    // the saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
-    currency: '€',
-    filename: `${utils.formatDate(new Date())}_${VENDOR}_${doc.amount.toFixed(
-      2
-    )}€${doc.vendorRef ? '_' + doc.vendorRef : ''}.jpg`,
-    vendor: VENDOR,
-    metadata: {
-      // it can be interesting that we add the date of import. This is not mandatory but may be
-      // useful for debugging or data migration
-      importDate: new Date(),
-      // document version, useful for migration after change of document structure
-      version: 1
-    }
-  }))
+async function getYears() {
+  try {
+    const urlYears = `${baseUrl}/prive/listeanneeremuneration/v1`
+    const req = await request({ uri: urlYears })
+    return JSON.parse(req).donnee
+  } catch (err) {
+    log('error', err.message)
+  }
 }
 
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
+async function fetchPayrolls(year) {
+  try {
+    // For each year, list all the available payrolls
+    const urlPayrolls = `${baseUrl}/prive/anneeremuneration/v1/${year}`
+    const req = await request({ uri: urlPayrolls })
+    const payrolls = JSON.parse(req).donnee
+    return payrolls
+  } catch (err) {
+    log('error', err.message)
+  }
+}
+
+async function parseDocuments() {
+  // Get the available years for payrolls
+  const years = await getYears()
+  const docs = []
+  for (const year of years) {
+    // Get all the payrolls for a year
+    const payrolls = await fetchPayrolls(year)
+    for (const payroll of payrolls) {
+      // For each payroll, get the pdf url and metadata
+      const uuid = payroll.documentUuid
+      const fileurl = `${baseUrl}/prive/telechargerdocumentremuneration/v1?documentUuid=${uuid}`
+      const filename = payroll.nomDocument
+      const date = new Date(payroll.dateDocument)
+      const amount = parseFloat(
+        payroll.libelle3.replace(' ', '').replace(',', '.')
+      )
+      const vendor = VENDOR
+      const doc = {
+        date,
+        fileurl,
+        filename,
+        amount,
+        vendor
+      }
+      docs.push(doc)
+    }
+  }
+  return docs
 }
