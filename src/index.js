65db2c1ff03312f27feeb7dbaa69cd9f1bceb645
LucsT <lucas@cozycloud.cc>
2019-05-03 10:50:37 +0200

    fix: Add 21 days to date for bills

--- src/index.js
+++ src/index.js
@@ -92,10 +92,15 @@ async function parseDocuments() {
       const uuid = payroll.documentUuid
       const fileurl = `${baseUrl}/prive/telechargerdocumentremuneration/v1?documentUuid=${uuid}`
       // Try to replace _XX_ known type_
-      let filename = payroll.nomDocument.replace(/_AF_/,'_Attestation fiscale_')
-      filename = filename.replace(/_BP_/,'_Bulletin de paie_')
-      filename = filename.replace(/_DR_/,'_Décompte de rappel_')
-      const date = new Date(payroll.dateDocument)
+      let filename = payroll.nomDocument.replace(
+        /_AF_/,
+        '_Attestation_fiscale_'
+      )
+      filename = filename.replace(/_BP_/, '_Bulletin_de_paie_')
+      filename = filename.replace(/_DR_/, '_Décompte_de_rappel_')
+      // Date is set to 22 of the month for easier matching, if not BP is always at 1st
+      let datePlus21 = new Date(payroll.dateDocument)
+      datePlus21.setDate(datePlus21.getDate() + 21)
       const amount = parseFloat(
         payroll.libelle3.replace(' ', '').replace(',', '.')
       )
@@ -111,7 +116,7 @@ async function parseDocuments() {
       } else if (payroll.icone === 'document') {
         // This doc have amount, it's a bill !
         const doc = {
-          date,
+          date: datePlus21,
           fileurl,
           filename,
           amount,
